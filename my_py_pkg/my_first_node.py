1acbd7bd808e027efff697b485402f3260f4a6ca
DavidGuamanDavila <adavidguamandavila@gmail.com>
2024-03-17 20:58:11 -0230

    Added counter times

--- my_py_pkg/my_first_node.py
+++ my_py_pkg/my_first_node.py
@@ -5,10 +5,14 @@ from rclpy.node import Node
 class MyNode(Node):
     def __init__(self):
         super().__init__("py_test")
+        self.counter_ = 0
         self.get_logger().info("Hello ROS2")
         self.create_timer(0.5, self.timer_callback)
+
+
     def timer_callback(self):
-        self.get_logger().info("Hello")
+        self.get_logger().info("Hello" + str(self.counter_))
+        self.counter_ += 1
 
 def main(args=None):
     rclpy.init(args=args)
